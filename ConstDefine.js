6eb99f104b7d3cd1bcf929d128d763d8c73da02d
teriyaki <kaisatec@users.noreply.github.com>
2021-05-25 15:20:33 -0700

    Add files via upload

---
+++ ConstDefine.js
@@ -0,0 +1,5 @@
+const Const_Customer="c";//table name
+const Const_IP="t";//table name
+const Const_SecretRoute="Grapetreetown306__";
+const Const_TimeOut=60;//second default 600
+module.exports = ConstDefine;
